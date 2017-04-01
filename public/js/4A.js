f94a0cf1a6e978122c8fb67ab7911c395cdd8c4e
Ritchie Anesco <ritchie.anesco@isobar.com>
2017-04-01 13:12:53 +1100

    Run production build in webpack

--- public/js/4A.js
+++ public/js/4A.js
@@ -1,6 +1,13 @@
 /* global React ReactDOM */
 
 /*Using webpack to compile all imports - cmd line is `webpack public/js/4A public/js/4` (webpack inputFile outputFile) */
+/*
+Production cmd
+NODE_ENV=production webpack -p public/js/4A.js public/js/4.js
+
+Develop cmd
+webpack public/js/4A public/js/4
+*/
 
 import React from 'react'
 import ReactDOM from 'react-dom'
