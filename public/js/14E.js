8d2d06c7bb0ceb55da50ef2af32437c68c3e2669
Ritchie Anesco <ritchie.anesco@isobar.com>
2017-04-02 20:52:36 +1000

    Use componentDidMount to fire off other events

--- public/js/14E.js
+++ public/js/14E.js
@@ -1,8 +1,13 @@
 /* Adding a header component */
 
 import React from 'react'
+
+// promise based http client
+import axios from 'axios'
 import Header from './14F'
 const { shape, string } = React.PropTypes
+
+// componentDidMount method - hook to call when component is loaded
 
 const Details = React.createClass({
   propTypes: {
@@ -11,17 +16,40 @@ const Details = React.createClass({
       year: string,
       poster: string,
       trailer: string,
-      description: string
+      description: string,
+      imdbID: string
     })
   },
+
+  getInitialState () {
+    return {
+      omdbData: {}
+    }
+  },
+
+  componentDidMount () {
+    axios.get(`http://www.omdbapi.com/?i=${this.props.show.imdbID}`)
+    .then((response) => {
+      this.setState({omdbData: response.data})
+    })
+    .catch((error) => console.error('axios error', error))
+  },
+
   render () {
     const { title, year, poster, trailer, description } = this.props.show
+    let rating
+    if (this.state.omdbData.imdbRating) {
+      rating = <h3>{this.state.omdbData.imdbRating}</h3>
+    } else {
+      rating = <img src='/public/img/loading.png' alt='loading indicator' />
+    }
     return (
       <div className='details'>
         <Header />
         <section>
           <h1>{title}</h1>
           <h2>({year})</h2>
+          {rating}
           <img src={`/public/img/posters/${poster}`} />
           <p>{description}</p>
         </section>
