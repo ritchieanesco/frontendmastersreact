cebe9e3bcfa43b27d95c2c2028f951c252a2648b
Ritchie Anesco <ritchie.anesco@isobar.com>
2017-04-02 12:44:59 +1000

    - Jest and Enzyme Snapshot Testing - Enzyme snapshot testing current component - Jest snapshots all imported component

---
+++ public/js/12B.js
@@ -0,0 +1,18 @@
+/* Filtering */
+
+import React from 'react'
+import { Link } from 'react-router'
+
+const Landing = React.createClass({
+  render () {
+    return (
+      <div className='landing'>
+        <h1>My Landing</h1>
+        <input type='text' placeholder='Search' />
+        <Link to='/search'>or Browse all</Link>
+      </div>
+    )
+  }
+})
+
+export default Landing
