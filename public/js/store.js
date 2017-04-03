338ad2eb10d872dffd2702b8fe40fac16cdeaf9e
Ritchie Anesco <ritchie.anesco@isobar.com>
2017-04-03 20:53:11 +1000

    Set up redux devtools

--- public/js/store.js
+++ public/js/store.js
@@ -1,6 +1,10 @@
-import { createStore } from 'redux'
+import { createStore, compose } from 'redux'
 import rootReducer from './reducers'
 
-const store = createStore(rootReducer)
+// activating redux devtools - make sure its in browser and has devtools for redux
+// (f) => f means do nothing / return what you are given
+const store = createStore(rootReducer, compose(
+  typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : (f) => f
+))
 
 export default store
